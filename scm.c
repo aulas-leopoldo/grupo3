350f0ee752c8983cddf6881b9acd8ab0782efcf4
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-08-10 16:46:36 -0300

    feat: add date and clock

--- scm.c
+++ scm.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
@@ -10,6 +11,20 @@ void multiplica(int a, int b) {
 
 int main() {
     printf("Olá, Estudante!\n");
+
+    //////////Aluno: Gabriel Oliveira / Alteração de data e hora
+    struct tm *data_hora_atual;
+    time_t segundos;
+    time(&segundos);  
+    data_hora_atual = localtime(&segundos);
+    
+    printf("Hoje é dia %d/%d/%d e hora atual é ", 
+    data_hora_atual->tm_mday, data_hora_atual->tm_mon+1,data_hora_atual->tm_year+1900 );
+    
+    printf("%d:",data_hora_atual->tm_hour);//hora   
+    printf("%d hrs \n",data_hora_atual->tm_min);//minuto
+    //////////Fim da alteração de data e hora
+
     soma(5, 3);
     multiplica(5, 3);
     return 0;
